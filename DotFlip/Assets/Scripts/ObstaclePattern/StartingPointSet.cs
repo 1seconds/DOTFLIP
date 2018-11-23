603d3801a2ee85e290a55ed6065be6e82ade6f9e
usung <1seconds@naver.com>
2018-11-23 23:44:09 +0900

    no message

--- DotFlip/Assets/Scripts/ObstaclePattern/StartingPointSet.cs
+++ DotFlip/Assets/Scripts/ObstaclePattern/StartingPointSet.cs
@@ -9,43 +9,144 @@ public class StartingPointSet : MonoBehaviour
     private float restY;
     private int modX;       //몫
     private int modY;
-    private CameraSystem cameraSystem;
 
     void Start ()
     {
-        cameraSystem = GameObject.FindWithTag("GameManager").GetComponent<CameraSystem>();
         tmpVector = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().WorldToScreenPoint(gameObject.transform.position);
         restX = tmpVector.x % 80;
         restY = tmpVector.y % 80;
         modX = (int)(tmpVector.x / 80);
         modY = (int)(tmpVector.y / 80);
+        
+        //아래방향
+        if(modY < 0)
+        {
+            Debug.Log(gameObject.name + " : " + modX + " " + modY);
+            if (restX < 40)
+            {
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.DOWNSIDE, modX - 1, modY + 8 , false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.DOWNSIDE, modX - 1, modY + 8, false);
+                }
+            }
+            else if (restX >= 40)
+            {
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.DOWNSIDE, modX, modY + 8, false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.DOWNSIDE, modX, modY + 8, false);
+                }
+            }
+            gameObject.transform.position += new Vector3(0, 0, 10);
+        }
 
-        if (restX < 40)
+        //위쪽
+        else if(modY > 6)
         {
-            if (restY < 40)
+            if (restX < 40)
             {
-                gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * modY, 0));
-                GameSystem.TileObject(cameraSystem.currentCameraView, modX - 1, modY - 1, false);
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.UPSIDE, modX - 1, modY - 1, false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.UPSIDE, modX - 1, modY, false);
+                }
             }
-            else if (restY >= 40)
+            else if (restX >= 40)
             {
-                gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * (modY + 1), 0));
-                GameSystem.TileObject(cameraSystem.currentCameraView, modX - 1, modY, false);
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.UPSIDE, modX, modY - 1, false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.UPSIDE, modX, modY, false);
+                }
             }
+            gameObject.transform.position += new Vector3(0, 0, 10);
         }
-        else if (restX >= 40)
+
+        //왼쪽
+        else if (modX > 13)
         {
-            if (restY < 40)
+            if (restX < 40)
             {
-                gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * modY, 0));
-                GameSystem.TileObject(cameraSystem.currentCameraView, modX, modY - 1, false);
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.RIGHTSIDE, modX - 1, modY - 1, false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.RIGHTSIDE, modX - 1, modY, false);
+                }
             }
-            else if (restY >= 40)
+            else if (restX >= 40)
             {
-                gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * (modY + 1), 0));
-                GameSystem.TileObject(cameraSystem.currentCameraView, modX, modY, false);
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.RIGHTSIDE, modX, modY - 1, false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.RIGHTSIDE, modX, modY, false);
+                }
             }
+            gameObject.transform.position += new Vector3(0, 0, 10);
         }
-        gameObject.transform.position += new Vector3(0, 0, 10);
+
+        //센터
+        else
+        {
+            if (restX < 40)
+            {
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.CENTER, modX - 1, modY - 1, false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * modX, 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.CENTER, modX - 1, modY, false);
+                }
+            }
+            else if (restX >= 40)
+            {
+                if (restY < 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * modY, 0));
+                    GameSystem.TileObject(CameraView.CENTER, modX, modY - 1, false);
+                }
+                else if (restY >= 40)
+                {
+                    gameObject.transform.position = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().ScreenToWorldPoint(new Vector3(80 * (modX + 1), 80 * (modY + 1), 0));
+                    GameSystem.TileObject(CameraView.CENTER, modX, modY, false);
+                }
+            }
+            gameObject.transform.position += new Vector3(0, 0, 10);
+        }
+
+        
     }
 }
