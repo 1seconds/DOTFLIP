19f2a3d381fb0b5fa84ecab7b46a2e6075a26e1e
usung <1seconds@naver.com>
2018-12-05 19:54:21 +0900

    [DEV] 3D

---
+++ DotFlip/Assets/Scenes/Scripts/ObstaclePattern/GoalManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalManager : MonoBehaviour
+{
+    string stage = "0";
+    private float time_ = 0;
+    private void OnTriggerStay(Collider obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            time_ += Time.deltaTime;
+            if(time_ > 2f)
+            {
+                if (System.Convert.ToInt32((GameObject.FindWithTag("GameManager").GetComponent<StageSystem>().currentStage+1).ToString()) < 10)
+                    stage += (GameObject.FindWithTag("GameManager").GetComponent<StageSystem>().currentStage+1).ToString();
+                else
+                    stage = (GameObject.FindWithTag("GameManager").GetComponent<StageSystem>().currentStage+1).ToString();
+                SceneManager.LoadScene(stage);
+            }
+            else
+                obj.GetComponent<PlayerMove>().currentDirect = Direct.HOLD;
+        }
+    }
+}
